ec5d9385d36117914cc507a5ec7204511538dde4
techstarteranhkim <anh.kim@docc.techstarter.de>
2024-01-23 14:30:44 +0100

    cleanup

--- scripts.js
+++ scripts.js
@@ -1,38 +1,36 @@
-// Funktion zur Berechnung des Alters
-function berechneAlter(geburtsdatum) {
-    var heute = new Date();
-    var geburtsdatum = new Date(geburtsdatum);
-    var alter = heute.getFullYear() - geburtsdatum.getFullYear();
-    var m = heute.getMonth() - geburtsdatum.getMonth();
-    if (m < 0 || (m === 0 && heute.getDate() < geburtsdatum.getDate())) {
-        alter--;
-    }
-    return alter;
+// Popups
+function signupSuccessful() {
+    alert('Signup successful');
 }
 
-// Funktion zur Überprüfung der Volljährigkeit
-function istVolljaehrig(geburtsdatum) {
-    var alter = berechneAlter(geburtsdatum);
-    if (alter >= 18) {
-        return true;
-    } else {
-        return false;
-    }
+function loginSuccessful() {
+    alert('Login successful');
 }
 
-// Funktion zur Überprüfung des Alters
-function checkAlter() {
-    // Geburtsdatum abrufen
-    var geburtsdatum = document.getElementById('geburtsdatum').value;
+//JSON files
+function submitSignupForm() {
+    event.preventDefault();
 
-    // Alter berechnen und in der Konsole ausgeben
-    var alter = berechneAlter(geburtsdatum);
-    console.log("Sie sind " + alter + " Jahre alt.");
+    var contact = {
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        name: document.getElementById('name').value
+    };
 
-    // Überprüfen, ob die Person volljährig ist
-    if (istVolljaehrig(geburtsdatum)) {
-        console.log("Sie sind volljährig.");
-    } else {
-        console.log("Sie sind nicht volljährig.");
-    }
+    console.log(contact);
+    window.location.href = 'index.html';
+    return false;
+}
+
+function submitLoginForm() {
+    event.preventDefault();
+
+    var contact = {
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+    };
+
+    console.log(contact);
+    window.location.href = 'index.html';
+    return false;
 }
